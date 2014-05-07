05209d829fefac6781ed0cde5325c3853e0ccee0
Tom Flannaghan <tomflannaghan@gmail.com>
2014-05-07 16:56:09 -0400

    Added the bullets

--- src/extension.js
+++ src/extension.js
@@ -25,6 +25,49 @@ function getSettings(schema) {
     return new Gio.Settings({ schema: schema });
 }
 
+// A menu item with a radio style bullet point that is right aligned.
+const RadioPopupMenuItem = new Lang.Class({
+    Name: 'RadioPopupMenuItem',
+    Extends: PopupMenu.PopupBaseMenuItem,
+
+    _init: function (text, params) {
+        this.parent(params);
+        
+        // the two labels used
+        this._label = new St.Label({ text: text });
+        this._radio = new St.Label({ text: "●" });
+        
+        // assemble everything
+        this.actor.add_child(this._label);
+        this._radioBin = new St.Bin({ x_align: St.Align.END });
+        this.actor.add(this._radioBin, { expand: true, x_align: St.Align.END });
+        this._radioBin.child = this._radio;
+        this.actor.label_actor = this._label
+        
+        // initial state is false
+        this.setToggleState(false);
+    },
+
+    activate: function(event) {
+        this.setToggleState(!this._state);
+        this.emit('toggled', this._state);
+        this.parent(event);
+    },
+
+    setToggleState: function(state) {
+        if (state) {
+            this._radio.show();
+            this._state = true;
+        }
+        else {
+            this._radio.hide();
+            this._state = false;
+        }
+    },
+    
+});
+
+
 function ProxyMenuButton() {
     this._init();
 }
@@ -64,21 +107,26 @@ ProxyMenuButton.prototype = {
         Main.panel._rightBox.insert_child_at_index(this.actor, 0);
 
         // putting the popup menu together
-        this._manualSwitch = new PopupMenu.PopupSwitchMenuItem(
-            _("Manual"), false, {});
-        this._autoSwitch = new PopupMenu.PopupSwitchMenuItem(
-            _("Automatic"), false, {});
+        this._noneSwitch = new RadioPopupMenuItem(
+            _("None"), {});
+        this._manualSwitch = new RadioPopupMenuItem(
+            _("Manual"), {});
+        this._autoSwitch = new RadioPopupMenuItem(
+            _("Automatic"), {});
+        this.menu.addMenuItem(this._noneSwitch);
         this.menu.addMenuItem(this._manualSwitch);
         this.menu.addMenuItem(this._autoSwitch);
+        this._noneSwitch.connect(
+            'toggled', Lang.bind(this, function() {
+                this._settings.set_string(PROXY_MODE, 'none');
+            }));
         this._manualSwitch.connect(
             'toggled', Lang.bind(this, function() {
-                this._settings.set_string(
-                    PROXY_MODE, this._mode == 'manual' ? 'none' : 'manual');
+                this._settings.set_string(PROXY_MODE, 'manual');
             }));
         this._autoSwitch.connect(
             'toggled', Lang.bind(this, function() {
-                this._settings.set_string(
-                    PROXY_MODE, this._mode == 'auto' ? 'none' : 'auto');
+                this._settings.set_string(PROXY_MODE, 'auto');
             }));
         // Add a menu seperator and link to network settings to bottom of menu
         this.menu.addMenuItem(new PopupMenu.PopupSeparatorMenuItem());
@@ -89,6 +137,7 @@ ProxyMenuButton.prototype = {
     
     refresh: function() {
         // run every time settings have changed.
+        this._noneSwitch.setToggleState(this._mode == 'none');
         this._manualSwitch.setToggleState(this._mode == 'manual');
         this._autoSwitch.setToggleState(this._mode == 'auto');
         switch (this._mode) {
