b0b980dbabc84b14dd9329707e53afe037d838b0
Tom Flannaghan <tomflannaghan@gmail.com>
2015-04-21 15:42:21 -0400

    Remove the print statement.

--- make_locale.py
+++ make_locale.py
@@ -6,5 +6,4 @@ UUID = "ProxySwitcher@flannaghan.com"
 for po in glob.glob("po/*.po"):
     lang = os.path.basename(po).rsplit('.', 1)[0]
     cmd = "mkdir -p locale/{0}/LC_MESSAGES; msgfmt {1} -o locale/{0}/LC_MESSAGES/{2}.mo".format(lang, po, UUID)
-    print cmd
     os.system(cmd)
