e49015bf74f778fa2e16c31e1301e4cf46707e45
Tom Flannaghan <tom@flannaghan.com>
2023-09-10 20:06:10 +0100

    Working - not pretty

--- src/extension.js
+++ src/extension.js
@@ -1,12 +1,9 @@
-const Main = imports.ui.main;
-const Gio = imports.gi.Gio;
-const PopupMenu = imports.ui.popupMenu;
-const QuickSettings = imports.ui.quickSettings;
-const QuickSettingsMenu = imports.ui.main.panel.statusArea.quickSettings;
-const ExtensionUtils = imports.misc.extensionUtils;
-const Me = ExtensionUtils.getCurrentExtension();
-const Gettext = imports.gettext.domain(Me.metadata.uuid);
-const _ = Gettext.gettext;
+import Gio from 'gi://Gio';
+import * as PopupMenu from 'resource:///org/gnome/shell/ui/popupMenu.js';
+import * as QuickSettings from 'resource:///org/gnome/shell/ui/quickSettings.js';
+import * as Main from 'resource:///org/gnome/shell/ui/main.js';
+import {Extension, gettext as _} from 'resource:///org/gnome/shell/extensions/extension.js';
+
 
 const PROXY_SCHEMA = "org.gnome.system.proxy"
 const PROXY_MODE = "mode"
@@ -17,16 +14,10 @@ const modeText = {'none': "Off",
                   'auto': "Automatic"};
 const modeList = ['none', 'manual', 'auto'];
 
-// These will store the objects we create on enable.
-let settings = null;
-let settingsConnectionId = null;
-let clickedConnectionId = null;
-let switcherMenu = null;
-let items = [];
 
 class ModeMenuItem {
     // A class that wraps a menu item associated with a proxy mode.
-    constructor(mode) {
+    constructor(mode, settings) {
         this.mode = mode;
         this.item = new PopupMenu.PopupMenuItem(_(modeText[mode]));
         this.connectionId = this.item.connect("activate", function() {
@@ -40,88 +31,98 @@ class ModeMenuItem {
     }
 }
 
-function init() {
-    ExtensionUtils.initTranslations(Me.metadata.uuid);
-}
 
-function enable() {
-    // connect to the gsettings proxy schema
-    if (Gio.Settings.list_schemas().indexOf(PROXY_SCHEMA) == -1)
-        throw _("Schema \"%s\" not found.").format(PROXY_SCHEMA);
-    settings =  new Gio.Settings({ schema: PROXY_SCHEMA });    
+export default class ProxySwitcherExtension extends Extension {
 
-    // make the menu
-    switcherMenu = new QuickSettings.QuickMenuToggle({
-        title: _("Proxy"),
-        iconName: "preferences-system-network-proxy-symbolic",
-    });
-    switcherMenu.menu.setHeader("preferences-system-network-proxy-symbolic", _("Proxy"));
+    enable() {
+        // connect to the gsettings proxy schema
+        if (Gio.Settings.list_schemas().indexOf(PROXY_SCHEMA) == -1)
+            throw _("Schema \"%s\" not found.").format(PROXY_SCHEMA);
+        this.settings =  new Gio.Settings({ schema: PROXY_SCHEMA });    
 
-    clickedConnectionId = switcherMenu.connect(
-        'clicked', () => switcherMenu.menu.open(),
-    );
+        // make the menu
+        this.switcherMenu = new QuickSettings.QuickMenuToggle({
+            title: _("Proxy"),
+            iconName: "preferences-system-network-proxy-symbolic",
+        });
+        this.switcherMenu.menu.setHeader("preferences-system-network-proxy-symbolic", _("Proxy"));
 
-    QuickSettingsMenu._addItems([switcherMenu]);
+        // make an "indicator" (whatever that is) and add our menu to it. This is how we add it
+        // to the actual system menu.
+        this.indicator = new QuickSettings.SystemIndicator(this);
+        this.indicator.quickSettingsItems.push(this.switcherMenu);
+        Main.panel.statusArea.quickSettings.addExternalIndicator(this.indicator);
 
-    // add menu item for each mode.
-    items = [];
-    for (const mode of modeList) {
-        const item = new ModeMenuItem(mode);
-        items.push(item);
-        switcherMenu.menu.addMenuItem(item.item);
-    }
+        this.clickedConnectionId = this.switcherMenu.connect(
+            'clicked', () => this.switcherMenu.menu.open(),
+        );
 
-    // Add a link to launch network settings.
-    switcherMenu.menu.addMenuItem(
-        new PopupMenu.PopupSeparatorMenuItem());
-    switcherMenu.menu.addSettingsAction(
-        _("Network Settings"), 'gnome-network-panel.desktop');
-
-    // Register callback for changes to the settings
-    settingsConnectionId = settings.connect(
-        'changed::' + PROXY_MODE, reflectSettings,
-    );
-
-    reflectSettings();
-}
-
-function reflectSettings() {
-    // Synchronises the menu indicator with the Gnome Settings,
-    // allowing us to reflect changes made externally to the extension.
-    const mode = settings.get_string(PROXY_MODE);
-    if (mode == "none") {
-        switcherMenu.checked = false;
-        switcherMenu.subtitle = null;    
-    } else {
-        switcherMenu.checked = true;
-        switcherMenu.subtitle = _(modeText[mode]);    
-    }
-    
-    for (const item of items) {
-        item.item.setOrnament(
-            (mode == item.mode) ? PopupMenu.Ornament.DOT
-                : PopupMenu.Ornament.NONE);
-    }
-}
-
-function disable() {
-    if (settings) {
-        if (settingsConnectionId) {
-            settings.disconnect(settingsConnectionId);
-            settingsConnectionId = null;
+        // add menu item for each mode.
+        this.items = [];
+        for (const mode of modeList) {
+            const item = new ModeMenuItem(mode);
+            this.items.push(item);
+            this.switcherMenu.menu.addMenuItem(item.item);
         }
-        settings = null;
+
+        // Add a link to launch network settings.
+        this.switcherMenu.menu.addMenuItem(
+            new PopupMenu.PopupSeparatorMenuItem());
+        this.switcherMenu.menu.addSettingsAction(
+            _("Network Settings"), 'gnome-network-panel.desktop');
+
+        // Register callback for changes to the settings
+        this.settingsConnectionId = this.settings.connect(
+            'changed::' + PROXY_MODE, this.reflectSettings,
+        );
+
+        this.reflectSettings();
     }
 
-    for (const item of items) {
-        item.destroy();
+    reflectSettings() {
+        // Synchronises the menu indicator with the Gnome Settings,
+        // allowing us to reflect changes made externally to the extension.
+        const mode = this.settings.get_string(PROXY_MODE);
+        if (mode == "none") {
+            this.switcherMenu.checked = false;
+            this.switcherMenu.subtitle = null;    
+        } else {
+            this.switcherMenu.checked = true;
+            this.switcherMenu.subtitle = _(modeText[mode]);    
+        }
+        
+        for (const item of this.items) {
+            item.item.setOrnament(
+                (mode == item.mode) ? PopupMenu.Ornament.DOT
+                    : PopupMenu.Ornament.NONE);
+        }
     }
-    items = [];
 
-    if (switcherMenu) {
-        switcherMenu.disconnect(clickedConnectionId);
-        clickedConnectionId = null;
-        switcherMenu.destroy();
-        switcherMenu = null
+    disable() {
+        if (this.settings) {
+            if (this.settingsConnectionId) {
+                this.settings.disconnect(this.settingsConnectionId);
+                this.settingsConnectionId = null;
+            }
+            this.settings = null;
+        }
+
+        for (const item of this.items) {
+            item.destroy();
+        }
+        this.items = [];
+
+        if (this.switcherMenu) {
+            this.switcherMenu.disconnect(this.clickedConnectionId);
+            this.clickedConnectionId = null;
+            this.switcherMenu.destroy();
+            this.switcherMenu = null
+        }
+
+        if (this.indicator) {
+            this.indicator.destroy();
+            this.indicator = null;
+        }
     }
+
 }
