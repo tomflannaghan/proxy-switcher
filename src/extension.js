49efad20cf48fbf25753db8df23f9ddbe445295a
Tom Flannaghan <tom@flannaghan.com>
2023-09-14 22:22:21 +0100

    Clean up the code, using a class for the toggle

--- src/extension.js
+++ src/extension.js
@@ -1,4 +1,6 @@
 import Gio from 'gi://Gio';
+import GObject from 'gi://GObject';
+
 import * as PopupMenu from 'resource:///org/gnome/shell/ui/popupMenu.js';
 import * as QuickSettings from 'resource:///org/gnome/shell/ui/quickSettings.js';
 import * as Main from 'resource:///org/gnome/shell/ui/main.js';
@@ -15,21 +17,62 @@ const modeText = {'none': "Off",
 const modeList = ['none', 'manual', 'auto'];
 
 
-class ModeMenuItem {
-    // A class that wraps a menu item associated with a proxy mode.
-    constructor(mode, settings) {
-        this.mode = mode;
-        this.item = new PopupMenu.PopupMenuItem(_(modeText[mode]));
-        this.connectionId = this.item.connect("activate", function() {
-            settings.set_string(PROXY_MODE, mode);
-        });
-    }
+const ProxyMenuToggle = GObject.registerClass(
+    class ProxyMenuToggle extends QuickSettings.QuickMenuToggle {
+        _init(extensionObject) {
+            super._init({
+                title: _("Proxy"),
+                iconName: "preferences-system-network-proxy-symbolic",
+            });
+    
+            // Add a header with an icon, title and optional subtitle. This is
+            // recommended for consistency with other quick settings menus.
+            this.menu.setHeader("preferences-system-network-proxy-symbolic", _("Proxy"));
+    
+            // Add a section of items to the menu
+            let itemsSection = new PopupMenu.PopupMenuSection();
+            this._modeToItem = {};
+            for (const mode of modeList) {
+                this._modeToItem[mode] = itemsSection.addAction(_(modeText[mode]),
+                    () => extensionObject.proxySettings.set_string(PROXY_MODE, mode));
+            }
+            this.menu.addMenuItem(itemsSection);
+    
+            this.menu.addMenuItem(new PopupMenu.PopupSeparatorMenuItem());
+            const settingsItem = this.menu.addSettingsAction(
+                _("Network Settings"), 'gnome-network-panel.desktop');
+    
+            // Ensure the settings are unavailable when the screen is locked
+            settingsItem.visible = Main.sessionMode.allowSettings;
+            this.menu._settingsActions[extensionObject.uuid] = settingsItem;
 
-    destroy() {
-        this.item.disconnect(this.connectionId);
-        this.item.destroy();
+            // Clean-up. Not really sure if this is necessary, but want to be safe.
+            this.connect('destroy', () => (this._modeToItem = null));
+
+            // Set the initial state.
+            this._reflectSettings(extensionObject);
+        }
+
+        _reflectSettings(extensionObject) {
+            // Synchronises the menu indicator with the Gnome Settings,
+            // allowing us to reflect changes made externally to the extension.
+            const mode = extensionObject.proxySettings.get_string(PROXY_MODE);
+            if (mode == "none") {
+                this.checked = false;
+                this.subtitle = null;    
+            } else {
+                this.checked = true;
+                this.subtitle = _(modeText[mode]);    
+            }
+            
+            for (const itemMode of modeList) {
+                this._modeToItem[itemMode].setOrnament(
+                    (mode == itemMode) ? PopupMenu.Ornament.DOT
+                        : PopupMenu.Ornament.NONE);
+            }
+        }
     }
-}
+);
 
 
 export default class ProxySwitcherExtension extends Extension {
@@ -38,91 +81,36 @@ export default class ProxySwitcherExtension extends Extension {
         // connect to the gsettings proxy schema
         if (Gio.Settings.list_schemas().indexOf(PROXY_SCHEMA) == -1)
             throw _("Schema \"%s\" not found.").format(PROXY_SCHEMA);
-        this.settings =  new Gio.Settings({ schema: PROXY_SCHEMA });    
+        this.proxySettings =  new Gio.Settings({ schema: PROXY_SCHEMA });    
 
         // make the menu
-        this.switcherMenu = new QuickSettings.QuickMenuToggle({
-            title: _("Proxy"),
-            iconName: "preferences-system-network-proxy-symbolic",
-        });
-        this.switcherMenu.menu.setHeader("preferences-system-network-proxy-symbolic", _("Proxy"));
-
-        // make an "indicator" (whatever that is) and add our menu to it. This is how we add it
-        // to the actual system menu.
-        this.indicator = new QuickSettings.SystemIndicator(this);
-        this.indicator.quickSettingsItems.push(this.switcherMenu);
-        Main.panel.statusArea.quickSettings.addExternalIndicator(this.indicator);
-
-        this.clickedConnectionId = this.switcherMenu.connect(
-            'clicked', () => this.switcherMenu.menu.open(),
-        );
-
-        // add menu item for each mode.
-        this.items = [];
-        for (const mode of modeList) {
-            const item = new ModeMenuItem(mode);
-            this.items.push(item);
-            this.switcherMenu.menu.addMenuItem(item.item);
-        }
-
-        // Add a link to launch network settings.
-        this.switcherMenu.menu.addMenuItem(
-            new PopupMenu.PopupSeparatorMenuItem());
-        this.switcherMenu.menu.addSettingsAction(
-            _("Network Settings"), 'gnome-network-panel.desktop');
+        let switcherMenu = new ProxyMenuToggle(this);
+        
+        // make an "indicator" (whatever that is) and add our menu to it. This is 
+        // how we add it to the actual system menu.
+        this._indicator = new QuickSettings.SystemIndicator(this);
+        this._indicator.quickSettingsItems.push(switcherMenu);
+        Main.panel.statusArea.quickSettings.addExternalIndicator(this._indicator);
 
         // Register callback for changes to the settings
-        this.settingsConnectionId = this.settings.connect(
-            'changed::' + PROXY_MODE, this.reflectSettings,
+        this.settingsConnectionId = this.proxySettings.connect(
+            'changed::' + PROXY_MODE, () => switcherMenu._reflectSettings(this),
         );
-
-        this.reflectSettings();
-    }
-
-    reflectSettings() {
-        // Synchronises the menu indicator with the Gnome Settings,
-        // allowing us to reflect changes made externally to the extension.
-        const mode = this.settings.get_string(PROXY_MODE);
-        if (mode == "none") {
-            this.switcherMenu.checked = false;
-            this.switcherMenu.subtitle = null;    
-        } else {
-            this.switcherMenu.checked = true;
-            this.switcherMenu.subtitle = _(modeText[mode]);    
-        }
-        
-        for (const item of this.items) {
-            item.item.setOrnament(
-                (mode == item.mode) ? PopupMenu.Ornament.DOT
-                    : PopupMenu.Ornament.NONE);
-        }
     }
 
     disable() {
-        if (this.settings) {
+        if (this.proxySettings) {
             if (this.settingsConnectionId) {
-                this.settings.disconnect(this.settingsConnectionId);
+                this.proxySettings.disconnect(this.settingsConnectionId);
                 this.settingsConnectionId = null;
             }
-            this.settings = null;
+            this.proxySettings = null;
         }
 
-        for (const item of this.items) {
-            item.destroy();
-        }
-        this.items = [];
-
-        if (this.switcherMenu) {
-            this.switcherMenu.disconnect(this.clickedConnectionId);
-            this.clickedConnectionId = null;
-            this.switcherMenu.destroy();
-            this.switcherMenu = null
-        }
-
-        if (this.indicator) {
-            this.indicator.destroy();
-            this.indicator = null;
+        if (this._indicator) {
+            this._indicator.quickSettingsItems.forEach(item => item.destroy());
+            this._indicator.destroy();
+            this._indicator = null;
         }
     }
-
 }
